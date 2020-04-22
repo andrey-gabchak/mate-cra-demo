a4c39cc75efe1f190a8d56f432ec80d182ccd048
Gabchak <andrey.gabchak@gmail.com>
2020-04-22 09:56:03 +0300

    Updates

---
+++ precache-manifest.64e6af44f24e2fb6c0d59d96997816dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af53525dd103d8c2c9a8815ae54f779",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51a24bfb692daab28030",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f5f440a4ab39975198ef",
+    "url": "./static/js/2.72886dcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.72886dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a24bfb692daab28030",
+    "url": "./static/js/main.f3e6e4bd.chunk.js"
+  },
+  {
+    "revision": "e9885517c90fd7c1b1e9",
+    "url": "./static/js/runtime-main.2f13b6e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
